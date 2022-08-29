502cb86bcbe49cc64cc657fbddeef461f7d1ed8a
Thành Bùi <thanhbui.230202@gmail.com>
2022-08-29 16:37:30 +0700

    update

---
+++ Bai3_Tuan1/app/src/main/java/caidat/dmt/buiminhthanh_2050531200332/MainActivity.java
@@ -0,0 +1,14 @@
+package caidat.dmt.buiminhthanh_2050531200332;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
